82cdcb6fb1882c9206afa0e41ea19be5bf40aa51
Miao Wang <shankerwangmiao@gmail.com>
2026-09-05 06:06:10 +0800

    fix(voice): disable or enable TONE/CSQL functions when setting CTCSS/DCS
    As required by hamlib, when disabling CTCSS or DCS, and the respective
    function is supported by the radio through rig_get/set_function, the
    user is expected to disable CTCSS or DCS by calling rig_set_function.
    When enabling CTCSS or DCS, the user is expected to enable the
    respective function by calling rig_set_function as well. When the
    function is not supported, the user is expected to write the respective
    tone or code as 0 to disable it, or a valid tone or code to enable it.
    
    This commit prevents from writing a tone or code of 0 to disable CTCSS
    or DCS when the respective function is supported to avoid possible
    errors when the radio does not accept a tone or code of 0 to disable
    CTCSS or DCS.

--- packages/server/src/radio/connections/HamlibConnection.ts
+++ packages/server/src/radio/connections/HamlibConnection.ts
@@ -3130,6 +3130,19 @@ export class HamlibConnection
     return this.runSerializedTask('getCtcssTone', async () => {
       this.checkConnected();
       try {
+        if (this.supportedFunctions.has('TONE')) {
+          const value = (await Promise.race([
+            this.rig!.getFunction('TONE'),
+            new Promise<never>((_, reject) =>
+              setTimeout(() => reject(new Error('Get CTCSS tone timeout')), 5000)
+            ),
+          ])) as boolean;
+          this.lastSuccessfulOperation = Date.now();
+          if (!value) {
+            // If the radio reports that CTCSS is disabled, return 0 to indicate no tone.
+            return 0;
+          }
+        }
         const value = (await Promise.race([
           this.rig!.getCtcssTone(),
           new Promise<never>((_, reject) =>
@@ -3148,6 +3161,17 @@ export class HamlibConnection
     await this.runSerializedTask('setCtcssTone', async () => {
       this.checkConnected();
       try {
+        if (this.supportedFunctions.has('TONE') && tone === 0) {
+          await Promise.race([
+            this.rig!.setFunction('TONE', false),
+            new Promise<never>((_, reject) =>
+              setTimeout(() => reject(new Error('Disable CTCSS tone timeout')), 5000)
+            ),
+          ]);
+          this.lastSuccessfulOperation = Date.now();
+          logger.debug('CTCSS tone disabled');
+          return;
+        }
         await Promise.race([
           this.rig!.setCtcssTone(tone),
           new Promise<never>((_, reject) =>
@@ -3156,6 +3180,16 @@ export class HamlibConnection
         ]);
         this.lastSuccessfulOperation = Date.now();
         logger.debug('CTCSS tone set', { tone });
+        if (this.supportedFunctions.has('TONE')) {
+          await Promise.race([
+            this.rig!.setFunction('TONE', true),
+            new Promise<never>((_, reject) =>
+              setTimeout(() => reject(new Error('Enable CTCSS tone timeout')), 5000)
+            ),
+          ]);
+          this.lastSuccessfulOperation = Date.now();
+          logger.debug('CTCSS tone enabled');
+        }
       } catch (error) {
         throw this.convertOptionalOperationError(error, 'setCtcssTone');
       }
@@ -3184,6 +3218,19 @@ export class HamlibConnection
     return this.runSerializedTask('getDcsCode', async () => {
       this.checkConnected();
       try {
+        if (this.supportedFunctions.has('CSQL')) {
+          const value = (await Promise.race([
+            this.rig!.getFunction('CSQL'),
+            new Promise<never>((_, reject) =>
+              setTimeout(() => reject(new Error('Get DCS code function timeout')), 5000)
+            ),
+          ])) as boolean;
+          this.lastSuccessfulOperation = Date.now();
+          if (!value) {
+            // If the radio reports that DCS is disabled, return 0 to indicate no code.
+            return 0;
+          }
+        }
         const value = (await Promise.race([
           this.rig!.getDcsCode(),
           new Promise<never>((_, reject) =>
@@ -3202,6 +3249,17 @@ export class HamlibConnection
     await this.runSerializedTask('setDcsCode', async () => {
       this.checkConnected();
       try {
+        if (this.supportedFunctions.has('CSQL') && code === 0) {
+          await Promise.race([
+            this.rig!.setFunction('CSQL', false),
+            new Promise<never>((_, reject) =>
+              setTimeout(() => reject(new Error('Disable DCS code function timeout')), 5000)
+            ),
+          ]);
+          this.lastSuccessfulOperation = Date.now();
+          logger.debug('DCS code disabled');
+          return;
+        }
         await Promise.race([
           this.rig!.setDcsCode(code),
           new Promise<never>((_, reject) =>
@@ -3210,6 +3268,16 @@ export class HamlibConnection
         ]);
         this.lastSuccessfulOperation = Date.now();
         logger.debug('DCS code set', { code });
+        if (this.supportedFunctions.has('CSQL')) {
+          await Promise.race([
+            this.rig!.setFunction('CSQL', true),
+            new Promise<never>((_, reject) =>
+              setTimeout(() => reject(new Error('Enable DCS code function timeout')), 5000)
+            ),
+          ]);
+          this.lastSuccessfulOperation = Date.now();
+          logger.debug('DCS code function enabled');
+        }
       } catch (error) {
         throw this.convertOptionalOperationError(error, 'setDcsCode');
       }
